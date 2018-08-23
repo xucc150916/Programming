0ad573d89c5583b586e2e14e71033ab90ea9b512
xuchen <1024436423@qq.com>
2018-08-23 22:29:49 +0800

    modify

--- Match.java
+++ Match.java
@@ -1,56 +1,54 @@
 /**
- * 正则表达式匹配问题
- *
  * 请实现一个函数用来匹配包括'.'和'*'的正则表达式。
  * 模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（包含0次）。
  * 在本题中，匹配是指字符串的所有字符匹配整个模式。
  * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配
  */
-public class Match {
-
+public class match {
     public static void main(String[] args) {
-        Match match = new Match();
-        System.out.println(match.match(",".toCharArray(), ".*".toCharArray()));
+        String str = "";
+        String pattern = ".";
+        System.out.println(match(str.toCharArray(), pattern.toCharArray()));
     }
 
-    public boolean match(char[] str, char[] pattern) {
-
-        return match(pattern, 0, str, 0);
-    }
-
-    public boolean match(char[] pattern, int patternIndex, char[] str, int strIndex) {
-
-        // 正则表达式跑完了，字符串没跑完
-        if (patternIndex==pattern.length && strIndex!=str.length) {
+    public static boolean match(char[] str, char[] pattern) {
+        if (str == null || pattern == null) {
             return false;
         }
 
-        // 字符串跑完了，正则表达式没跑完
-        if (patternIndex!=pattern.length && strIndex==str.length) {
-            return false;
-        }
+        return matchCore(str, 0, pattern, 0);
+    }
 
-        // 两者都跑完了
-        if (patternIndex==pattern.length && strIndex==str.length) {
+    public static boolean matchCore(char[] str, int strIndex, char[] pattern, int patternIndex) {
+
+        // 同时结束
+        if (strIndex == str.length && patternIndex == pattern.length) {
             return true;
         }
 
-        // 正则表达式下一个字符存在，且为 "*"
-        if (patternIndex+1<pattern.length && pattern[patternIndex+1] == '*') {
-            // 当前正则表达式为 "." 或者字符匹配
-            if (str[strIndex]==pattern[patternIndex] || pattern[patternIndex]=='.') {
-                return match(pattern, patternIndex, str, strIndex+1)
-                    || match(pattern, patternIndex+2, str, strIndex+1)
-                    || match(pattern, patternIndex+2, str, strIndex);
+        // str是不会先于pattern结束的，这种情况不考虑，只考虑pattern先结束的情况
+        if (strIndex != str.length && patternIndex == pattern.length) {
+            return false;
+        }
+
+
+        // pattern下一个为 '*'
+        if (patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
+            if (strIndex != str.length && (pattern[patternIndex] == str[strIndex] || pattern[patternIndex] == '.')) {
+                // 当前字符匹配
+                return matchCore(str, strIndex + 1, pattern, patternIndex + 2) ||
+                        matchCore(str, strIndex + 1, pattern, patternIndex) ||
+                        matchCore(str, strIndex, pattern, patternIndex + 2);
             }
-            return match(pattern, patternIndex+1, str, strIndex);
+            return matchCore(str, strIndex, pattern, patternIndex + 2);
         }
 
-        // 判断
-        if (pattern[patternIndex]==str[strIndex] || pattern[patternIndex]=='.') {
-            return match(pattern, patternIndex+1, str, strIndex+1);
+        // 字符
+        if (strIndex != str.length && (pattern[patternIndex] == str[strIndex] || pattern[patternIndex] == '.')) {
+            return matchCore(str, strIndex + 1, pattern, patternIndex + 1);
         }
-
         return false;
     }
+
+
 }
